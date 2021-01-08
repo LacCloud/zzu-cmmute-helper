0f98d80736406a17df5931e1c27240a969227453
LacCloud <75606188+LacCloud@users.noreply.github.com>
2021-01-08 15:29:16 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -15,5 +15,5 @@ url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 curl -d "day6=b&did=1&men6=a&ptopid=${ptopid}&sid=${sid}" ${url}
 #进行打卡
 
-myvs_13c=河南省.郑州市.金水区
+myvs_13c=河南省.郑州市.郑州大学.主校区
 curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=${myvs_13c}&myvs_14=否&did=2&day6=b&men6=a&ptopid=${ptopid}&sid={$sid}" ${url}
