0642e50f3c4b3adc0842c3602f61f7027c11459e
LacCloud <75606188+LacCloud@users.noreply.github.com>
2021-01-11 15:13:27 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 uid=$1
 upw=$2
+myvs_13c=$3
 
 #获取ptopid和sid
 curl -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o "udata.txt"
@@ -15,5 +16,5 @@ url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 curl -d "day6=b&did=1&men6=a&ptopid=${ptopid}&sid=${sid}" ${url}
 #进行打卡
 
-myvs_13c=河南省.郑州市.郑州大学.主校区
+myvs_13c=$myvs_13c&
 curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=${myvs_13c}&myvs_14=否&did=2&day6=b&men6=a&ptopid=${ptopid}&sid={$sid}" ${url}
