a632f65dada1074200c165579628622fc34166ab
LacCloud <75606188+LacCloud@users.noreply.github.com>
2021-01-12 12:50:39 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 uid=$1
 upw=$2
-myvs_13c=$3
+address=$3
 smbtn=进入健康状况上报平台
 
 #获取ptopid和sid
@@ -17,5 +17,5 @@ url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 curl -d "day6=b&did=1&men6=a&ptopid=${ptopid}&sid=${sid}" ${url}
 #进行打卡
 
-myvs_13c=${myvs_13c}&
+myvs_13b=address
 curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=${myvs_13c}&myvs_14=否&did=2&day6=b&men6=a&ptopid=${ptopid}&sid={$sid}" ${url}
