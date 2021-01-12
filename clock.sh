0034d69a43a322410a1b16936b70f7546013b6f1
LacCloud <75606188+LacCloud@users.noreply.github.com>
2021-01-12 12:43:07 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -2,10 +2,11 @@
 uid=$1
 upw=$2
 myvs_13c=$3
+smbtn=进入健康状况上报平台
 
 #获取ptopid和sid
-curl -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o "udata.txt"
-udata=$(sed -n '11p' udata.txt)
+curl -d "uid=${uid}&upw=${upw}&smbtn=${smbtn}&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o temp.txt
+udata=$(sed -n '11p' temp.txt)
 udata=${udata#*ptopid=}
 udata=${udata%\"\}\}*}
 ptopid="${udata%&*}"
